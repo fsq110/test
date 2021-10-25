c7bf88f38245cd3fffefed522687ff0aa2fa4824
fsq110 <93119041+fsq110@users.noreply.github.com>
2021-10-25 16:03:53 +0800

    Create helloworld_test.go

---
+++ helloworld_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	if ans := Add(1, 2); ans != 3 {
+		t.Errorf("1 + 2 expected be 3, but %d got", ans)
+	} else {
+		t.Log("ans = ", ans)
+	}
+
+	if ans := Add(-10, -20); ans != -30 {
+		t.Errorf("-10 + -20 expected be -30, but %d got", ans)
+	} else {
+		t.Log("ans = ", ans)
+	}
+}
